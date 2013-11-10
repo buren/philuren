c2787403f57f0f7fced0475fdd93340bcfeef626
Jacob Burenstam <burenstam@gmail.com>
2013-11-10 12:17:07 +0100

    Added particle 3d wave animation

--- public/client.js
+++ public/client.js
@@ -8,6 +8,25 @@ window.onload = function() {
   initTimeline();
   initWink();
 
+  $('a.anchor-link').click(function(e){
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 1000);
+    return false;
+  });
+
+
+  particlesAnimated = false;
+  $('#particle-animation-trigger').waypoint(function() {
+    if (particlesAnimated)
+      return;
+    var wave = document.querySelector(".particles");
+    wave.setAttribute("class", "particles animate");
+
+    particlesAnimated = true;
+  });
+
   cubeInitialized = false;
   $('#simple-hack').waypoint(function() {
     initCube();
